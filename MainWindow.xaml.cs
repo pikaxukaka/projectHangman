06ae7121dc9241358aeb270e61c32cff04219441
Pikaxukaka <pikaxukaka@gmail.com>
2023-02-22 20:50:20 +0100

    Dodaj pliki projektów.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace projektWisielec
+{
+    public partial class MainWindow : Window
+    {
+        private List<string> words = new List<string>()
+        {
+            "abstrakcja",
+            "dyskrecja",
+            "fotografia",
+            "programowanie",
+            "komputer",
+            "klawiatura",
+            "mikrofon",
+            "telewizja"
+        };
+
+        private string currentWord;
+        private List<char> correctLetters = new List<char>();
+        private int lives = 6;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            StartGame();
+        }
+
+        private void StartGame()
+        {
+            Random random = new Random();
+            currentWord = words[random.Next(words.Count)];
+            WordTextBlock.Text = new string('_', currentWord.Length);
+            correctLetters.Clear();
+            lives = 6;
+            UpdateResultTextBlock();
+        }
+
+        private void GuessButton_Click(object sender, RoutedEventArgs e)
+        {
+            string input = InputTextBox.Text.Trim().ToLower();
+
+            if (string.IsNullOrEmpty(input))
+            {
+                return;
+            }
+
+            if (input.Length == 1)
+            {
+                if (currentWord.Contains(input[0]))
+                {
+                    correctLetters.Add(input[0]);
+                    UpdateWordTextBlock();
+                    if (IsGameWon())
+                    {
+                        ShowMessage("You won!");
+                        StartGame();
+                    }
+                }
+                else
+                {
+                    lives--;
+                    UpdateResultTextBlock();
+                    if (lives == 0)
+                    {
+                        ShowMessage("You lost!");
+                        StartGame();
+                    }
+                }
+            }
+            else
+            {
+                if (currentWord == input)
+                {
+                    WordTextBlock.Text = currentWord;
+                    ShowMessage("You won!");
+                    StartGame();
+                }
+                else
+                {
+                    lives--;
+                    UpdateResultTextBlock();
+                    if (lives == 0)
+                    {
+                        ShowMessage("You lost!");
+                        StartGame();
+                    }
+                }
+            }
+
+            InputTextBox.Clear();
+        }
+
+        private void UpdateWordTextBlock()
+        {
+            char[] word = new char[currentWord.Length];
+            for (int i = 0; i < currentWord.Length; i++)
+            {
+                if (correctLetters.Contains(currentWord[i]))
+                {
+                    word[i] = currentWord[i];
+                }
+                else
+                {
+                    word[i] = '_';
+                }
+            }
+            WordTextBlock.Text = new string(word);
+        }
+
+        private bool IsGameWon()
+        {
+            foreach (char c in currentWord)
+            {
+                if (!correctLetters.Contains(c))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private void UpdateResultTextBlock()
+        {
+            ResultTextBlock.Text = $"Lives: {lives}";
+        }
+
+        private void ShowMessage(string message)
+        {
+            MessageBox.Show(message, "Game Over", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+    }
+}
